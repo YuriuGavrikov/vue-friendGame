d46c67d0fe576fddc2a8449d573400fc2f32db36
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2022-08-30 15:21:16 +0300

    create game

---
+++ script.js
@@ -0,0 +1,38 @@
+const App = {
+   data() {
+      return {
+         title: 'Game',
+         placeholderString: 'Введите текст',
+         userNameValue: '',
+         inputValue: '',
+         noteItem: {},
+         notes: [],
+      }
+   },
+   methods: {
+      addNewNote() {
+
+         if (this.inputValue !== '') {
+            let obj = {
+               visible: true,
+               text: this.inputValue
+            }
+
+            this.notes.push(obj)
+
+            this.inputValue = ''
+         }
+      },
+      removeNote(i, event) {
+         this.notes.splice(i, 1)
+      }
+   },
+   computed: {
+
+   },
+   watch: {
+
+   }
+}
+
+Vue.createApp(App).mount('#app')
