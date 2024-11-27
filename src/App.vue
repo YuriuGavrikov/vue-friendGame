6a775b63d1e8c91fb6f00cd0b3106a8ee59c7a6f
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2024-11-28 01:03:31 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import Game from './components/Game.vue'
+import Game from "./components/Game.vue";
 </script>
 
 <template>
   <Game />
+  <!-- Yandex.RTB R-A-13092515-1 -->
+  <div id="yandex_rtb_R-A-13092515-1"></div>
+  <script>
+    window.yaContextCb.push(() => {
+      Ya.Context.AdvManager.render({
+        blockId: "R-A-13092515-1",
+        renderTo: "yandex_rtb_R-A-13092515-1",
+        type: "feed",
+      });
+    });
+  </script>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
